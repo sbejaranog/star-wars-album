3a5b0494a2ecd8834cd5724b7f1bee555d7e6eed
s.bejarano.g00 <s.bejarano.g00@gmail.com>
2024-08-08 06:05:15 +0000

    feat: changed configurations of the app.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,24 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', 'index.html'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+	theme: {
+    extend: {
+      colors: {
+        starwars: {
+          primary: '#ffcc00',
+          secondary: '#1a1a1a',
+          buttonPrimary: '#d32f2f',
+          buttonSecondary: '#1976d2',
+          buttonDisabled: '#9e9e9e',
+        },
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled'],
+    },
   },
   plugins: [],
 };
